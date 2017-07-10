b5b888e12deebd2a71e5453164f834357e70d398
JHades <jhades.dev@gmail.com>
2017-07-10 16:49:31 +0200

    angular http post

--- http-sample-app/src/app/app.component.ts
+++ http-sample-app/src/app/app.component.ts
@@ -1,7 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs/Observable";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpRequest} from "@angular/common/http";
 import * as _ from 'lodash';
+
+
+interface Course {
+    description: string;
+    courseListIcon:string;
+    iconUrl:string;
+    longDescription:string;
+    url:string;
+}
 
 @Component({
   selector: 'app-root',
@@ -19,18 +28,26 @@ import * as _ from 'lodash';
   `})
 export class AppComponent implements OnInit {
 
-    courses$: Observable<any>;
+    courses$: Observable<Course>;
 
     constructor(private http:HttpClient) {
 
     }
 
     ngOnInit() {
-
+/*
         this.courses$ = this.http
             .get("https://angular-http-guide.firebaseio.com/courses.json")
-            .map(res => _.values(res))
+            .do(console.log)
+            .map(data => _.values(data))
+
+*/
+
+        this.courses$ = this.http
+            .request<Object>(new HttpRequest('GET', "https://angular-http-guide.firebaseio.com/courses.json", {responseType:'json'}))
+            .map(data => _.values(data))
             .do(console.log);
+
 
     }
 
